d1b3d189f89be005686d86729c267fdf3b9d91bc
liangxiegame <liangxiegame@163.com>
2016-05-31 13:36:45 +0800

    AssetBundle练习

---
+++ Editor/BuildAssetBundle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class BuildAssetBundle : MonoBehaviour {
+
+	[MenuItem("QFramework/BuildAssetBundleCube")]
+	static void BuildAssetBundleCube()
+	{
+		Object prefab = AssetDatabase.LoadMainAssetAtPath ("Assets/QFramework/Cube.prefab");
+
+		string savePath = Application.dataPath + "/StreamingAssets/cube.assetbundle";
+
+		BuildPipeline.BuildAssetBundle (prefab,null,savePath,BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets,
+			BuildTarget.StandaloneOSXUniversal);
+	}
+
+	[MenuItem("QFramework/BuildAssetBundleMusic")]
+	static void BuildAssetBundleMusic()
+	{
+		Object prefab = AssetDatabase.LoadMainAssetAtPath ("Assets/QFramework/Test.mp3");
+
+		string savePath = Application.dataPath + "/StreamingAssets/test.assetbundle";
+
+		BuildPipeline.BuildAssetBundle (prefab,null,savePath,BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets,
+			BuildTarget.StandaloneOSXUniversal);
+	}
+
+	[MenuItem("QFramework/BuildAssetBundleScene")]
+	static void ExportScene()
+	{
+		string[] scenes = { "Assets/QFramework/Scene.unity" };
+
+		string savePath = Application.dataPath + "/StreamingAssets/scene.assetbundle";
+
+		BuildPipeline.BuildStreamedSceneAssetBundle (scenes, savePath, BuildTarget.StandaloneOSXIntel);
+	}
+}
--- Example/Script/Load.cs
+++ Example/Script/Load.cs
@@ -1,27 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+
 
 public class Load : MonoBehaviour {
+
+	[SerializeField]
+	AudioSource mAudioSource;
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (LoadPrefab ());
 	}
 
+
+	void OnGUI()
+	{
+		if (GUILayout.Button ("LoadScene")) {
+			StartCoroutine (LoadScene ());
+		}
+	}
+
+
 	IEnumerator LoadPrefab()
 	{
-
-//		#if unity_android
-//		string url = Application.streamingAssetsPath + "/cube.assetbundle");
-//		#elif
-//		string url = "file://" + Application.streamingAssetsPath + "/cube.assetbundle");
-//		#endif
 
 		WWW download = new WWW ("file://" + Application.streamingAssetsPath + "/cube.assetbundle");
 		yield return download;
 
-		GameObject.Instantiate(download.assetBundle.LoadAsset<GameObject>("cube"));
+//		GameObject.Instantiate(download.assetBundle.LoadAsset<GameObject>("cube"));
+		GameObject.Instantiate(download.assetBundle.mainAsset);
+
+		yield return LoadMusic ();
 	}
-	
+
+	IEnumerator LoadMusic()
+	{
+		WWW download = new WWW ("file://" + Application.streamingAssetsPath + "/test.assetbundle");
+		yield return download;
+
+		mAudioSource.clip = download.assetBundle.mainAsset as AudioClip;
+
+		mAudioSource.Play ();
+	}
+
+	IEnumerator LoadScene()
+	{
+		WWW download = new WWW ("file://" + Application.streamingAssetsPath + "/scene.assetbundle");
+		yield return download;
+
+		// 一定要Log一次 否则会报错 Bug
+		Debug.LogWarning (download.assetBundle.name);
+		SceneManager.LoadScene ("Scene");
+	}
 
 }
