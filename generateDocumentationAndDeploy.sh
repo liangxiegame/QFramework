08d1af534cf940d89f3a8d2a1de1a38bc12d7563
liangxieq <liangxiegame@163.com>
2017-10-27 10:23:47 +0800

    0.0.0.18 hooks and ci merge;

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -59,7 +59,7 @@ git config user.email "travis@travis-ci.org"
 # stayed the same and will only update the changed files. So the gh-pages branch
 # can be safely cleaned, and it is sure that everything pushed later is the new
 # documentation.
-rm -rf *
+rm -rf html
 
 # Need to create a .nojekyll file to allow filenames starting with an underscore
 # to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
