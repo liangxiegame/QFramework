19ca85ee145df84390f0af714f04f1010fe2b79f
liangxieq <liangxiegame@163.com>
2017-10-28 23:28:56 +0800

    0.0.0.46 txt -> md

--- ci/githooks/folder_tree_generator.sh
+++ ci/githooks/folder_tree_generator.sh
@@ -1,3 +1,3 @@
 #!bin/sh
 
-/usr/local/bin/tree -I 'mkdocs.*|*.meta|docs|ci|README.md|dist|*.mp3|*.jpg|QFramework.sln|*.userprefs|*.csproj|Properties|*.txt|*.sh' > tree.txt
+/usr/local/bin/tree -I 'mkdocs.*|*.meta|docs|ci|README.md|dist|*.mp3|*.jpg|QFramework.sln|*.userprefs|*.csproj|Properties|*.txt|*.sh' > tree.md
