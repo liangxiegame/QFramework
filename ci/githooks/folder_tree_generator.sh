65de45f662e5bad6f708cfee9aa72a128a5ac13b
liangxieq <liangxiegame@163.com>
2017-10-28 22:43:50 +0800

    0.0.0.44 fix tree generator.sh

--- ci/githooks/folder_tree_generator.sh
+++ ci/githooks/folder_tree_generator.sh
@@ -1,4 +1,5 @@
 #!bin/sh
 
-tree -I 'mkdocs.*|*.meta|docs|ci|README.md|dist|*.mp3|*.jpg|QFramework.sln|*.userprefs|*.csproj|Properties|*.txt|*.sh' > tree.txt
+/usr/local/bin/tree -I 'mkdocs.*|*.meta|docs|ci|README.md|dist|*.mp3|*.jpg|QFramework.sln|*.userprefs|*.csproj|Properties|*.txt|*.sh' > tree.txt
 open tree.txt
+open ./
