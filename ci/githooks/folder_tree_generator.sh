2eb0768fcaafe914d6b08ef6faabb355bc682506
liangxieq <liangxiegame@163.com>
2017-10-29 00:59:58 +0800

    0.0.0.54 fix bug

--- ci/githooks/folder_tree_generator.sh
+++ ci/githooks/folder_tree_generator.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 
 # /usr/local/bin/tree -I 'mkdocs.*|*.meta|docs|ci|README.md|dist|*.mp3|*.jpg|QFramework.sln|*.userprefs|*.csproj|Properties|*.txt|*.sh' > tree.md
-cd QFramework
-/usr/local/bin/md-file-tree > ../docs/folder_tree.md
+# will add some feature in future
+/usr/local/bin/md-file-tree > tree.md
