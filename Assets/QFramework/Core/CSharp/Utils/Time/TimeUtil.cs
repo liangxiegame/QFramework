677379e3926b35e5a73f49bda9ac63199b27e014
liangxieq <liangxiegame@163.com>
2017-12-14 22:38:17 +0800

    0.0.1.29 Utils文件整理完毕

--- Assets/QFramework/Core/CSharp/Utils/Time/TimeUtil.cs
+++ Assets/QFramework/Core/CSharp/Utils/Time/TimeUtil.cs
@@ -25,13 +25,23 @@
 
 namespace QFramework
 {
-    public class ObjectIsNotRetainedByOwnerExceptionWithHint : ExceptionWithHint
-    {
-        public ObjectIsNotRetainedByOwnerExceptionWithHint(object obj, object owner)
-            : base("'" + owner + "' cannot release " + obj + "!\n" +
-                   "Object is not retained by this object!",
-                "An entity can only be released from objects that retain it.")
-        {
-        }    
-    }
+	using System;
+
+	public static class TimeUtil
+	{
+		/// 获取当前时间戳
+		/// </summary>
+		/// <param name="tenBits">为真时获取10位时间戳,为假时获取13位时间戳.</param>
+		/// <returns></returns>
+		public static int GetTimeStamp(bool tenBits = true)
+		{
+			TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+			long ret;
+			if (tenBits)
+				ret = Convert.ToInt64(ts.TotalSeconds);
+			else
+				ret = Convert.ToInt64(ts.TotalMilliseconds);
+			return (int) ret;
+		}
+	}
 }
