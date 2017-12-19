e4735445977bdc8ae941a992853785fd8a3533c3
liangxieq <liangxiegame@163.com>
2017-12-19 13:06:50 +0800

    0.0.1.34 CETR

--- Assets/QFramework/Core/Chaining/Extensions/Transform.cs
+++ Assets/QFramework/Core/Chaining/Extensions/Transform.cs
@@ -38,16 +38,61 @@ namespace QFramework
 	public static class TransformExtension
 	{
 		/// <summary>
-		/// 缓存的一些变量
+		/// 缓存的一些变量,免得每次声明
 		/// </summary>
 		private static Vector3 mLocalPos;
-		private static Vector3 mLocalScale;
+		private static Vector3 mScale;
+		private static Vector3 mPos;
+		
+		#region CETR001 Parent
+
+		public static T Parent<T>(this T selfComponent, Transform parent) where T : Component
+		{
+			selfComponent.transform.SetParent(parent);
+			return selfComponent;
+		}
+
+		#endregion
+
+		#region CETR002 LocalIdentity
 
 		public static T LocalIdentity<T>(this T selfComponent) where T : Component
 		{
 			selfComponent.transform.localPosition = Vector3.zero;
 			selfComponent.transform.localRotation = Quaternion.identity;
 			selfComponent.transform.localScale = Vector3.one;
+			return selfComponent;
+		}
+
+		#endregion
+
+		#region CETR003 LocalPosition
+
+		public static T LocalPosition<T>(this T selfComponent, Vector3 localPos) where T : Component
+		{
+			selfComponent.transform.localPosition = localPos;
+			return selfComponent;
+		}
+
+		public static Vector3 GetLocalPosition<T>(this T selfComponent) where T : Component
+		{
+			return selfComponent.transform.localPosition;
+		}
+		
+
+
+		public static T LocalPosition<T>(this T selfComponent, float x, float y, float z) where T : Component
+		{
+			selfComponent.transform.localPosition = new Vector3(x, y, z);
+			return selfComponent;
+		}
+		
+		public static T LocalPosition<T>(this T selfComponent, float x, float y) where T : Component
+		{
+			mLocalPos = selfComponent.transform.localPosition;
+			mLocalPos.x = x;
+			mLocalPos.y = y;
+			selfComponent.transform.localPosition = mLocalPos;
 			return selfComponent;
 		}
 
@@ -74,31 +119,50 @@ namespace QFramework
 			selfComponent.transform.localPosition = mLocalPos;
 			return selfComponent;
 		}
-
-		public static T PositionX<T>(this T selfComponent, float x) where T : Component
+		
+		
+		public static T LocalPositionIdentity<T>(this T selfComponent) where T : Component
 		{
-			var pos = selfComponent.transform.position;
-			pos.x = x;
-			selfComponent.transform.position = pos;
+			selfComponent.transform.localPosition = Vector3.one;
 			return selfComponent;
 		}
 
-		public static T PositionY<T>(this T selfComponent, float y) where T : Component
+		#endregion
+
+		#region CETR004 LocalRotation
+
+		public static Quaternion GetLocalRotation<T>(this T selfComponent) where T : Component
 		{
-			var pos = selfComponent.transform.position;
-			pos.y = y;
-			selfComponent.transform.position = pos;
+			return selfComponent.transform.localRotation;
+		}
+
+		public static T LocalRotation<T>(this T selfComponent, Quaternion localRotation) where T : Component
+		{
+			selfComponent.transform.localRotation = localRotation;
 			return selfComponent;
 		}
 
-		public static T PositionZ<T>(this T selfComponent, float z) where T : Component
+		public static T LocalRotationIdentity<T>(this T selfComponent) where T : Component
 		{
-			var pos = selfComponent.transform.position;
-			pos.z = z;
-			selfComponent.transform.position = pos;
+			selfComponent.transform.localRotation = Quaternion.identity;
 			return selfComponent;
 		}
 
+		#endregion
+		
+		#region CETR005 LocalScale
+
+		public static T LocalScale<T>(this T selfComponent, Vector3 scale) where T : Component
+		{
+			selfComponent.transform.localScale = scale;
+			return selfComponent;
+		}
+
+		public static Vector3 GetLocalScale<T>(this T selfComponent) where T : Component
+		{
+			return selfComponent.transform.localScale;
+		}
+		
 		public static T LocalScale<T>(this T selfComponent, float xyz) where T : Component
 		{
 			selfComponent.transform.localScale = Vector3.one * xyz;
@@ -107,45 +171,192 @@ namespace QFramework
 
 		public static T LocalScale<T>(this T selfComponent, float x, float y, float z) where T : Component
 		{
-			mLocalScale = selfComponent.transform.localScale;
-			mLocalScale.x = x;
-			mLocalScale.y = y;
-			mLocalScale.z = z;
-			selfComponent.transform.localScale = mLocalScale;
+			mScale = selfComponent.transform.localScale;
+			mScale.x = x;
+			mScale.y = y;
+			mScale.z = z;
+			selfComponent.transform.localScale = mScale;
 			return selfComponent;
 		}
 
 		public static T LocalScale<T>(this T selfComponent, float x, float y) where T : Component
 		{
-			mLocalScale = selfComponent.transform.localScale;
-			mLocalScale.x = x;
-			mLocalScale.y = y;
-			selfComponent.transform.localScale = mLocalScale;
+			mScale = selfComponent.transform.localScale;
+			mScale.x = x;
+			mScale.y = y;
+			selfComponent.transform.localScale = mScale;
 			return selfComponent;
 		}
 
 		public static T LocalScaleX<T>(this T selfComponent, float x) where T : Component
 		{
-			mLocalScale = selfComponent.transform.localScale;
-			mLocalScale.x = x;
-			selfComponent.transform.localScale = mLocalScale;
+			mScale = selfComponent.transform.localScale;
+			mScale.x = x;
+			selfComponent.transform.localScale = mScale;
 			return selfComponent;
 		}
 
 		public static T LocalScaleY<T>(this T selfComponent, float y) where T : Component
 		{
-			mLocalScale = selfComponent.transform.localScale;
-			mLocalScale.y = y;
-			selfComponent.transform.localScale = mLocalScale;
+			mScale = selfComponent.transform.localScale;
+			mScale.y = y;
+			selfComponent.transform.localScale = mScale;
 			return selfComponent;
 		}
-
+		
 		public static T LocalScaleIdentity<T>(this T selfComponent) where T : Component
 		{
 			selfComponent.transform.localScale = Vector3.one;
 			return selfComponent;
 		}
+		
+		#endregion
 
+		#region CETR006 Identity
+
+		public static T Identity<T>(this T selfComponent) where T : Component
+		{
+			selfComponent.transform.position = Vector3.zero;
+			selfComponent.transform.rotation = Quaternion.identity;
+			selfComponent.transform.localScale = Vector3.one;
+			return selfComponent;
+		}
+
+		#endregion
+
+		#region CETR007 Position
+
+		public static T Position<T>(this T selfComponent, Vector3 position) where T : Component
+		{
+			selfComponent.transform.position = position;
+			return selfComponent;
+		}
+		
+		public static Vector3 GetPosition<T>(this T selfComponent) where T : Component
+		{
+			return selfComponent.transform.position;
+		}
+
+		public static T Position<T>(this T selfComponent, float x, float y, float z) where T : Component
+		{
+			selfComponent.transform.position = new Vector3(x, y, z);
+			return selfComponent;
+		}
+		
+		public static T Position<T>(this T selfComponent, float x, float y) where T : Component
+		{
+			mPos = selfComponent.transform.position;
+			mPos.x = x;
+			mPos.y = y;
+			selfComponent.transform.position = mPos;
+			return selfComponent;
+		}
+
+		public static T PositionIdentity<T>(this T selfComponent) where T : Component
+		{
+			selfComponent.transform.position = Vector3.zero;
+			return selfComponent;
+		}
+		
+		public static T PositionX<T>(this T selfComponent, float x) where T : Component
+		{
+			mPos = selfComponent.transform.position;
+			mPos.x = x;
+			selfComponent.transform.position = mPos;
+			return selfComponent;
+		}
+
+		public static T PositionY<T>(this T selfComponent, float y) where T : Component
+		{
+			mPos = selfComponent.transform.position;
+			mPos.y = y;
+			selfComponent.transform.position = mPos;
+			return selfComponent;
+		}
+
+		public static T PositionZ<T>(this T selfComponent, float z) where T : Component
+		{
+			mPos = selfComponent.transform.position;
+			mPos.z = z;
+			selfComponent.transform.position = mPos;
+			return selfComponent;
+		}
+
+		#endregion
+		
+		#region CETR008 Rotation
+
+		public static T RotationIdentity<T>(this T selfComponent) where T : Component
+		{
+			selfComponent.transform.rotation = Quaternion.identity;
+			return selfComponent;
+		}
+		
+		public static T Rotation<T>(this T selfComponent, Quaternion rotation) where T : Component
+		{
+			selfComponent.transform.rotation = rotation;
+			return selfComponent;
+		}
+
+		public static Quaternion GetRotation<T>(this T selfComponent) where T : Component
+		{
+			return selfComponent.transform.rotation;
+		}
+
+		#endregion
+
+
+		#region CETR009 Destroy All Child
+		
+		public static T DestroyAllChild<T>(this T selfComponent) where T : Component
+		{
+			var childCount = selfComponent.transform.childCount;
+
+			for (var i = 0; i < childCount; i++)
+			{
+				selfComponent.transform.GetChild(i).DestroyGameObjGracefully();
+			}
+
+			return selfComponent;
+		}
+
+		public static GameObject DestroyAllChild(this GameObject selfGameObj)
+		{
+			var childCount = selfGameObj.transform.childCount;
+
+			for (var i = 0; i < childCount; i++)
+			{
+				selfGameObj.transform.GetChild(i).DestroyGameObjGracefully();
+			}
+
+			return selfGameObj;
+		}
+
+		#endregion
+
+		#region CETR0010 Sibling Index
+
+		public static T AsLastSibling<T>(this T selfComponent) where T : Component
+		{
+			selfComponent.transform.SetAsLastSibling();
+			return selfComponent;
+		}
+
+		public static T AsFirstSibling<T>(this T selfComponent) where T : Component
+		{
+			selfComponent.transform.SetAsFirstSibling();
+			return selfComponent;
+		}
+
+		public static T SiblingIndex<T>(this T selfComponent, int index) where T : Component
+		{
+			selfComponent.transform.SetSiblingIndex(index);
+			return selfComponent;
+		}
+
+		#endregion
+		
+		
 		public static Transform FindByPath(this Transform selfTrans, string path)
 		{
 			return selfTrans.Find(path.Replace(".", "/"));
@@ -181,133 +392,12 @@ namespace QFramework
 			return selfComponent;
 		}
 
-		public static T LocalScale<T>(this T selfComponent, Vector3 scale) where T : Component
-		{
-			selfComponent.transform.localScale = scale;
-			return selfComponent;
-		}
-
-		public static Vector3 GetLocalScale<T>(this T selfComponent) where T : Component
-		{
-			return selfComponent.transform.localScale;
-		}
-		
-
-		public static Quaternion GetLocalRotation<T>(this T selfComponent) where T : Component
-		{
-			return selfComponent.transform.localRotation;
-		}
-
-		public static T LocalRotation<T>(this T selfComponent, Quaternion localRotation) where T : Component
-		{
-			selfComponent.transform.localRotation = localRotation;
-			return selfComponent;
-		}
-		
-		
-		public static T LocalRotationIdentity<T>(this T selfComponent) where T : Component
-		{
-			selfComponent.transform.localRotation = Quaternion.identity;
-			return selfComponent;
-		}
-
-		public static Vector3 GetLocalPosition<T>(this T selfComponent) where T : Component
-		{
-			return selfComponent.transform.localPosition;
-		}
-		
-		public static T LocalPositionIdentity<T>(this T selfComponent) where T : Component
-		{
-			selfComponent.transform.localPosition = Vector3.one;
-			return selfComponent;
-		}
-
-		public static T LocalPosition<T>(this T selfComponent, Vector3 localPos) where T : Component
-		{
-			selfComponent.transform.localPosition = localPos;
-			return selfComponent;
-		}
-
-		public static Vector3 GetPosition<T>(this T selfComponent) where T : Component
-		{
-			return selfComponent.transform.position;
-		}
-
-		public static T Position<T>(this T selfComponent, Vector3 position) where T : Component
-		{
-			selfComponent.transform.position = position;
-			return selfComponent;
-		}
-
-		public static T PositionIdentity<T>(this T selfComponent) where T : Component
-		{
-			selfComponent.transform.position = Vector3.zero;
-			return selfComponent;
-		}
-
-		public static Quaternion GetRotation<T>(this T selfComponent) where T : Component
-		{
-			return selfComponent.transform.rotation;
-		}
-
-		public static T Rotation<T>(this T selfComponent, Quaternion rotation) where T : Component
-		{
-			selfComponent.transform.rotation = rotation;
-			return selfComponent;
-		}
-
 		public static void CopyDataFromTrans(this Transform selfTrans, Transform fromTrans)
 		{
 			selfTrans.SetParent(fromTrans.parent);
 			selfTrans.localPosition = fromTrans.localPosition;
 			selfTrans.localRotation = fromTrans.localRotation;
 			selfTrans.localScale = fromTrans.localScale;
-		}
-
-		public static T Parent<T>(this T selfComponent, Transform parent) where T : Component
-		{
-			selfComponent.transform.SetParent(parent);
-			return selfComponent;
-		}
-
-		public static T DestroyAllChild<T>(this T selfComponent) where T : Component
-		{
-			foreach (Transform childTrans in selfComponent.transform)
-			{
-				childTrans.DestroyGameObjGracefully();
-			}
-			
-			return selfComponent;
-		}
-		
-		
-		public static GameObject DestroyAllChild(this GameObject selfGameObj)
-		{
-			foreach (Transform childTrans in selfGameObj.transform)
-			{
-				childTrans.DestroyGameObjGracefully();
-			}
-			
-			return selfGameObj;
-		}
-
-		public static T AsLastSibling<T>(this T selfComponent) where T : Component
-		{
-			selfComponent.transform.SetAsLastSibling();
-			return selfComponent;
-		}		
-		
-		public static T AsFirstSibling<T>(this T selfComponent) where T : Component
-		{
-			selfComponent.transform.SetAsFirstSibling();
-			return selfComponent;
-		}
-		
-
-		public static T SiblingIndex<T>(this T selfComponent,int index) where T : Component
-		{
-			selfComponent.transform.SetSiblingIndex(index);
-			return selfComponent;
 		}
 	}
 }
