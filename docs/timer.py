210e2acbcc4b13c7fc3fc33417d2bce00b7877b7
liangxiegame <liangxiegame@163.com>
2018-02-04 14:08:17 +0800

    add timer.py

--- docs/timer.py
+++ docs/timer.py
@@ -5,18 +5,18 @@ import time
 import subprocess
 
 def f():
-    print time.ctime(),'这是子进程,每10S执行一次'
+    subprocess.call("git pull", shell=True)
 
 def work():
    while 1:
        f()
-       time.sleep(10)
+       time.sleep(2)
 
 if __name__ == '__main__':
     p = multiprocessing.Process(target=work,)
     p.start()
     p.deamon = True
-    while 1:
-        print '这是主进程,每1秒执行一次'
-        subprocess.call("git pull", shell=True)
-        time.sleep(1)
+    #while 1:
+    #    print '这是主进程,每1秒执行一次'
+    #    subprocess.call("git pull", shell=True)
+    #    time.sleep(1)
