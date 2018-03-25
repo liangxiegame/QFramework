6e296f3a28a5c402c0bc2c5d852fb6b987a51804
liuzhenhua <liuzhenhua@putao.com>
2018-03-25 18:43:24 +0800

    0.0.4.20 代码规范化。

--- Assets/QFramework/Framework/3.UI/21.Guide/Scripts/CircleGuideCtrl.cs
+++ Assets/QFramework/Framework/3.UI/21.Guide/Scripts/CircleGuideCtrl.cs
@@ -1,10 +1,31 @@
 ﻿/****************************************************************************
- * Copyright (c) 2017 liuzhenhua@putao.com
+ * Copyright (c) 2018.3 dtknowlove@qq.com
+ * 
+ * http://qframework.io
+ * https://github.com/liangxiegame/QFramework
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  ****************************************************************************/
  
 using UnityEngine;
 
-namespace QFramework.Guide
+namespace QFramework
 {
 	/// <summary>
 	/// 圆形遮罩引导
@@ -14,51 +35,51 @@ namespace QFramework.Guide
 		/// <summary>
 		/// 镂空区域半径
 		/// </summary>
-		private float _radius;
+		private float mRadius;
 		/// <summary>
 		/// 当前高亮区域的半径
 		/// </summary>
-		private float _currentRadius = 0f;
+		private float mCurrentRadius = 0f;
 		
 		protected override void SetMatShader()
 		{
-			_shaderName = "UI/Guide/CircleGuide";
+			mShaderName = "UI/Guide/CircleGuide";
 		}
 
 		protected override void InitData()
 		{
 			Target.rectTransform.GetWorldCorners(_corners);
 			//获取最终高亮区域半径
-			_radius = Vector2.Distance(World2CanvasPos(_canvas, _corners[0]), World2CanvasPos(_canvas, _corners[3])) / 2f;
+			mRadius = Vector2.Distance(World2CanvasPos(mCanvas, _corners[0]), World2CanvasPos(mCanvas, _corners[3])) / 2f;
 			//计算圆心
 			float x = _corners[0].x + (_corners[3].x - _corners[0].x) / 2f;
 			float y = _corners[0].y + (_corners[1].y - _corners[0].y) / 2f;
 			Vector3 centerWorld = new Vector3(x, y, 0);
-			Vector2 center = World2CanvasPos(_canvas, centerWorld);
+			Vector2 center = World2CanvasPos(mCanvas, centerWorld);
 			//Apply 设置数据到shader中
 			Vector4 centerMat = new Vector4(center.x, center.y, 0, 0);
-			_material.SetVector("_Center", centerMat);
+			mMaterial.SetVector("_Center", centerMat);
 			//计算当前高亮显示区域半径
-			RectTransform canvasRectTransform = _canvas.transform as RectTransform;
+			RectTransform canvasRectTransform = mCanvas.transform as RectTransform;
 			canvasRectTransform.GetWorldCorners(_corners);
 			foreach (Vector3 corner in _corners)
 			{
-				_currentRadius = Mathf.Max(Vector3.Distance(World2CanvasPos(_canvas, corner), corner), _currentRadius);
+				mCurrentRadius = Mathf.Max(Vector3.Distance(World2CanvasPos(mCanvas, corner), corner), mCurrentRadius);
 			}
-			float initRadius = ShowAnim ? _currentRadius : _radius;
-			_material.SetFloat("_Slider", initRadius);
+			float initRadius = ShowAnim ? mCurrentRadius : mRadius;
+			mMaterial.SetFloat("_Slider", initRadius);
 		}
 
-		private float _shrinkVelocity = 0f;
+		private float shrinkVelocity = 0f;
 		protected override void PlayShrinkAnim()
 		{
 			if (!ShowAnim)
 				return;
-			float value = Mathf.SmoothDamp(_currentRadius, _radius, ref _shrinkVelocity, ShrinkTime);
-			if (!Mathf.Approximately(value, _currentRadius))
+			float value = Mathf.SmoothDamp(mCurrentRadius, mRadius, ref shrinkVelocity, ShrinkTime);
+			if (!Mathf.Approximately(value, mCurrentRadius))
 			{
-				_currentRadius = value;
-				_material.SetFloat("_Slider", _currentRadius);
+				mCurrentRadius = value;
+				mMaterial.SetFloat("_Slider", mCurrentRadius);
 			}
 		}
 	}
--- Assets/QFramework/Framework/3.UI/21.Guide/Scripts/RectGuideCtrl.cs
+++ Assets/QFramework/Framework/3.UI/21.Guide/Scripts/RectGuideCtrl.cs
@@ -1,36 +1,57 @@
 ﻿/****************************************************************************
- * Copyright (c) 2017 liuzhenhua@putao.com
+ * Copyright (c) 2018.3 dtknowlove@qq.com
+ * 
+ * http://qframework.io
+ * https://github.com/liangxiegame/QFramework
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  ****************************************************************************/
 using UnityEngine;
 
-namespace QFramework.Guide
+namespace QFramework
 {
 	public class RectGuideCtrl : GuideCtrl
 	{
 		/// <summary>
 		/// 镂空区域中心
 		/// </summary>
-		private Vector4 _center;
+		private Vector4 mCenter;
 		/// <summary>
 		/// 最终的偏移值X
 		/// </summary>
-		private float _targetOffsetX = 0f;
+		private float mTargetOffsetX = 0f;
 		/// <summary>
 		/// 最终的偏移值Y
 		/// </summary>
-		private float _targetOffsetY = 0f;
+		private float mTargetOffsetY = 0f;
 		/// <summary>
 		/// 当前的偏移值X
 		/// </summary>
-		private float _currentOffsetX = 0f;
+		private float mCurrentOffsetX = 0f;
 		/// <summary>
 		/// 当前的偏移值Y
 		/// </summary>
-		private float _currentOffsetY = 0f;
+		private float mCurrentOffsetY = 0f;
 
 		protected override void SetMatShader()
 		{
-			_shaderName = "UI/Guide/RectGuide";
+			mShaderName = "UI/Guide/RectGuide";
 		}
 
 		protected override void InitData()
@@ -38,18 +59,18 @@ namespace QFramework.Guide
 			//获取高亮区域四个顶点的世界坐标
 			Target.rectTransform.GetWorldCorners(_corners);
 			//计算高亮显示区域咋画布中的范围
-			_targetOffsetX = Vector2.Distance(World2CanvasPos(_canvas, _corners[0]), World2CanvasPos(_canvas, _corners[3])) / 2f;
-			_targetOffsetY = Vector2.Distance(World2CanvasPos(_canvas, _corners[0]), World2CanvasPos(_canvas, _corners[1])) / 2f;
+			mTargetOffsetX = Vector2.Distance(World2CanvasPos(mCanvas, _corners[0]), World2CanvasPos(mCanvas, _corners[3])) / 2f;
+			mTargetOffsetY = Vector2.Distance(World2CanvasPos(mCanvas, _corners[0]), World2CanvasPos(mCanvas, _corners[1])) / 2f;
 			//计算高亮显示区域的中心
 			float x = _corners[0].x + ((_corners[3].x - _corners[0].x) / 2f);
 			float y = _corners[0].y + ((_corners[1].y - _corners[0].y) / 2f);
 			Vector3 centerWorld = new Vector3(x,y,0);
-			Vector2 center = World2CanvasPos(_canvas, centerWorld);
+			Vector2 center = World2CanvasPos(mCanvas, centerWorld);
 			//设置遮罩材料中中心变量
 			Vector4 centerMat = new Vector4(center.x,center.y,0,0);
-			_material.SetVector("_Center",centerMat);
+			mMaterial.SetVector("_Center",centerMat);
 			//计算当前偏移的初始值
-			RectTransform canvasRectTransform = (_canvas.transform as RectTransform);
+			RectTransform canvasRectTransform = (mCanvas.transform as RectTransform);
 			if (canvasRectTransform != null)
 			{
 				//获取画布区域的四个顶点
@@ -58,37 +79,37 @@ namespace QFramework.Guide
 				for (int i = 0; i < _corners.Length; i++)
 				{
 					if (i % 2 == 0)
-						_currentOffsetX = Mathf.Max(Vector3.Distance(World2CanvasPos(_canvas, _corners[i]), center), _currentOffsetX);
+						mCurrentOffsetX = Mathf.Max(Vector3.Distance(World2CanvasPos(mCanvas, _corners[i]), center), mCurrentOffsetX);
 					else
-						_currentOffsetY = Mathf.Max(Vector3.Distance(World2CanvasPos(_canvas, _corners[i]), center), _currentOffsetY);
+						mCurrentOffsetY = Mathf.Max(Vector3.Distance(World2CanvasPos(mCanvas, _corners[i]), center), mCurrentOffsetY);
 				}
 			}
 			//设置遮罩材质中当前偏移的变量
-			float initX = ShowAnim ? _currentOffsetX : _targetOffsetX;
-			float initY = ShowAnim ? _currentOffsetY : _targetOffsetY;
-			_material.SetFloat("_SliderX",initX);
-			_material.SetFloat("_SliderY",initY);
+			float initX = ShowAnim ? mCurrentOffsetX : mTargetOffsetX;
+			float initY = ShowAnim ? mCurrentOffsetY : mTargetOffsetY;
+			mMaterial.SetFloat("_SliderX",initX);
+			mMaterial.SetFloat("_SliderY",initY);
 		}
 		
-		private float _shrinkVelocityX = 0f;
-		private float _shrinkVelocityY = 0f;
+		private float shrinkVelocityX = 0f;
+		private float shrinkVelocityY = 0f;
 		protected override void PlayShrinkAnim()
 		{
 			if (!ShowAnim)
 				return;
 			//从当前偏移值到目标偏移值差值显示收缩动画
-			float valueX = Mathf.SmoothDamp(_currentOffsetX, _targetOffsetX, ref _shrinkVelocityX, ShrinkTime);
-			float valueY = Mathf.SmoothDamp(_currentOffsetY, _targetOffsetY, ref _shrinkVelocityY, ShrinkTime);
-			if (!Mathf.Approximately(valueX, _currentOffsetX))
+			float valueX = Mathf.SmoothDamp(mCurrentOffsetX, mTargetOffsetX, ref shrinkVelocityX, ShrinkTime);
+			float valueY = Mathf.SmoothDamp(mCurrentOffsetY, mTargetOffsetY, ref shrinkVelocityY, ShrinkTime);
+			if (!Mathf.Approximately(valueX, mCurrentOffsetX))
 			{
-				_currentOffsetX = valueX;
-				_material.SetFloat("_SliderX",_currentOffsetX);
+				mCurrentOffsetX = valueX;
+				mMaterial.SetFloat("_SliderX",mCurrentOffsetX);
 			}
 
-			if (!Mathf.Approximately(valueY, _currentOffsetY))
+			if (!Mathf.Approximately(valueY, mCurrentOffsetY))
 			{
-				_currentOffsetY = valueY;
-				_material.SetFloat("_SliderY",_currentOffsetY);
+				mCurrentOffsetY = valueY;
+				mMaterial.SetFloat("_SliderY",mCurrentOffsetY);
 			}
 		}
 	}
