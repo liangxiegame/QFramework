d1d77ce1d6880f658bef99b9c7d9c8e33df4372f
karsion <karsion@126.com>
2018-05-06 10:38:46 +0800

    更新检视面板的辅助工具

--- Assets/QFramework/Framework/3.UI/EditorInspector/Editor/Editor/SelectionHelper.cs
+++ Assets/QFramework/Framework/3.UI/EditorInspector/Editor/Editor/SelectionHelper.cs
@@ -37,69 +37,61 @@ namespace QFramework
         [InitializeOnLoadMethod]
         private static void Start()
         {
-            //在Hierarchy面板按鼠标中键相当于开关GameObject
-            //In the Hierarchy panel, the middle mouse button is the equivalent of the toggle GameObject active.
-            EditorApplication.hierarchyWindowItemOnGUI = (instanceID, selectionRect) =>
-            {
-                Event e = Event.current;
-                if (e.type == EventType.KeyDown)
-                {
-                    switch (e.keyCode)
-                    {
-                        case KeyCode.Space:
-                            ToggleGameObjcetActiveSelf();
-                            e.Use();
-                            break;
-                        case KeyCode.Pause:
-                            Selection.gameObjects.ForEach(go => Debug.Log(go.transform.GetPath()));
-                            e.Use();
-                            break;
-                    }
-                }
-                else if (e.type == EventType.MouseDown && e.button == 2)
-                {
-                    ToggleGameObjcetActiveSelf();
-                    e.Use();
-                }
-            };
-
-            //在Project面板按鼠标中键相当于Show In Explorer
-            //In the Project panel, the middle mouse button is equivalent to "Show In Explorer".
-            EditorApplication.projectWindowItemOnGUI = (guid, selectionRect) =>
-            {
-                if (Event.current.type == EventType.MouseDown
-                    && Event.current.button == 2
-                    && selectionRect.Contains(Event.current.mousePosition))
-                {
-                    string strPath = AssetDatabase.GUIDToAssetPath(guid);
-                    if (Event.current.alt)
-                    {
-                        Debug.Log(strPath);
-                        Event.current.Use();
-                        return;
-                    }
-
-                    if (Path.GetExtension(strPath) == string.Empty) //文件夹
-                    {
-                        Process.Start(Path.GetFullPath(strPath));
-                    }
-                    else //文件
-                    {
-                        Process.Start("explorer.exe", "/select," + Path.GetFullPath(strPath));
-                    }
-
-                    Event.current.Use();
-                }
-            };
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGui;
+            EditorApplication.projectWindowItemOnGUI += ProjectWindowItemOnGui;
         }
 
-        internal static void ToggleGameObjcetActiveSelf()
+        //在Hierarchy面板按鼠标中键相当于开关GameObject
+        private static void HierarchyWindowItemOnGui(int instanceID, Rect selectionRect)
         {
-            Undo.RecordObjects(Selection.gameObjects, "Active");
-            Selection.gameObjects.ForEach(go =>
+            Event e = Event.current;
+            if ((e.type == EventType.KeyDown && e.keyCode == KeyCode.Space) || (e.type == EventType.MouseDown && e.button == 2))
             {
-                go.SetActive(!go.activeSelf);
-            });
+                if (e.alt)
+                {
+                    Selection.gameObjects.ForEach(go => Debug.Log(go.transform.GetPath()));
+                }
+                else
+                {
+                    Undo.RecordObjects(Selection.gameObjects, "Active");
+                    Selection.gameObjects.ForEach(go =>
+                    {
+                        go.SetActive(!go.activeSelf);
+                    });
+                }
+
+                e.Use();
+            }
+        }
+
+        //在Project面板按鼠标中键相当于Show In Explorer
+        //In the Project panel, the middle mouse button is equivalent to "Show In Explorer".
+        private static void ProjectWindowItemOnGui(string guid, Rect selectionRect)
+        {
+            Event e = Event.current;
+            if (e.type == EventType.MouseDown
+                && e.button == 2
+                && selectionRect.Contains(e.mousePosition))
+            {
+                string strPath = AssetDatabase.GUIDToAssetPath(guid);
+                if (e.alt)
+                {
+                    Debug.Log(strPath);
+                    Event.current.Use();
+                    return;
+                }
+
+                if (Path.GetExtension(strPath) == string.Empty) //文件夹
+                {
+                    Process.Start(Path.GetFullPath(strPath));
+                }
+                else //文件
+                {
+                    Process.Start("explorer.exe", "/select," + Path.GetFullPath(strPath));
+                }
+
+                e.Use();
+            }
         }
     }
 }
