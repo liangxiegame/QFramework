3468797310240ce7e77414dcb2b09334ee5e8cfe
liangxieq <liangxiegame@163.com>
2018-06-04 15:36:15 +0800

    0.0.7.2 fix compile error on windows

--- Assets/QFramework/Framework/4.EditorKit/Framework/PlatformUtils.cs
+++ Assets/QFramework/Framework/4.EditorKit/Framework/PlatformUtils.cs
@@ -40,12 +40,11 @@ namespace QFramework
 #if !UNITY_2017_3_OR_NEWER 
 			case BuildTarget.StandaloneOSXIntel:
 			case BuildTarget.StandaloneOSXIntel64:
-#endif
-#if UNITY_5 
+#elif UNITY_5 
 			case BuildTarget.StandaloneOSXUniversal:
 #else
             case BuildTarget.StandaloneOSX:
-			#endif
+#endif
 				return "OSX";
 				// Add more build targets for your own.
 				// If you add more targets, don't forget to add the same platforms to GetPlatformForAssetBundles(RuntimePlatform) function.
