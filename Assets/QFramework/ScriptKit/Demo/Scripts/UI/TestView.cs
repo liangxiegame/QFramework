41e61ebf2fa36416629e5c9c5ac4903d066ec3d2
Vin129 <515019721@qq.com>
2019-04-27 19:36:21 +0800

    repair tolua demo

---
+++ Assets/QFramework/ScriptKit/Demo/Scripts/UI/TestView.cs
@@ -0,0 +1,53 @@
+/****************************************************************************
+ * 2019.4 vin129的MacBook Pro
+ ****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using QFramework;
+
+namespace QFramework.Example
+{
+	public class TestViewData : UIPanelData
+	{
+		// TODO: Query Mgr's Data
+	}
+
+	public partial class TestView : UILuaPanel
+	{
+		protected override void ProcessMsg (int eventId,QMsg msg)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		protected override void OnInit(IUIData uiData = null)
+		{
+			mData = uiData as TestViewData ?? new TestViewData();
+			//please add init code here
+			BindLuaComponent();
+		}
+
+		protected override void OnOpen(IUIData uiData = null)
+		{
+		}
+
+		protected override void OnShow()
+		{
+		}
+
+		protected override void OnHide()
+		{
+		}
+
+		protected override void OnClose()
+		{
+		}
+
+		void ShowLog(string content)
+		{
+			Debug.Log("[ TestView:]" + content);
+		}
+	}
+}
