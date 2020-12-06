121f1e0995b2e7058cebba52d7bc0930ad90b489
liangxie <liangxiegame@163.com>
2020-12-06 18:45:18 +0800

    v0.10.44 添加一部分注释，更新 CSharpExtensions

--- Framework/1.Core/CSharpExtension/QFramework.CSharpExtension.cs
+++ Framework/1.Core/CSharpExtension/QFramework.CSharpExtension.cs
@@ -2595,6 +2595,26 @@ namespace QFramework
             var finalKeyIndex = values.GetRandomWithPower();
             return keys[finalKeyIndex];
         }
+        
+    }
+
+    public static class MaterialExtensions
+    {
+        /// <summary>
+        /// 参考资料: https://blog.csdn.net/qiminixi/article/details/78402505
+        /// </summary>
+        /// <param name="self"></param>
+        public static void SetStandardMaterialToTransparentMode(this Material self)
+        {
+            self.SetFloat("_Mode", 3);
+            self.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            self.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            self.SetInt("_ZWrite", 0);
+            self.DisableKeyword("_ALPHATEST_ON");
+            self.EnableKeyword("_ALPHABLEND_ON");
+            self.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            self.renderQueue = 3000;
+        }
     }
 #endif
 }
