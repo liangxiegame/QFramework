ead068c127828db10cccab8d89b8a67540c93e2e
liangxie <liangxiegame@163.com>
2020-12-31 23:33:36 +0800

    v0.11.4 BindableProperty => dll

--- QFramework/QFramework.Unity.Runtime/BindableProperty/QFramework.BindableProperty.cs
+++ QFramework/QFramework.Unity.Runtime/BindableProperty/QFramework.BindableProperty.cs
@@ -1,9 +1,33 @@
+/****************************************************************************
+ * Copyright (c) 2017 ~ 2020.12 liangxie
+ * 
+ * https://qframework.cn
+ * https://github.com/liangxiegame/QFramework
+ * https://gitee.com/liangxiegame/QFramework
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ****************************************************************************/
+
 using System;
 
-#if UNITY_5_6_OR_NEWER
 using UnityEngine;
 using UnityEngine.Events;
-#endif
 
 namespace QFramework
 {
@@ -89,9 +113,9 @@ namespace QFramework
             if (mSetter != null)
             {
                 mSetter.Invoke(mValue);
-#if UNITY_5_6_OR_NEWER
+
                 OnValueChanged.Invoke(mValue);
-#endif
+
             }
         }
 
@@ -133,9 +157,7 @@ namespace QFramework
 
         private event Action<T> mSetter = t => { };
 
-#if UNITY_5_6_OR_NEWER
         public UnityEvent<T> OnValueChanged = new OnPropertyChangedEvent<T>();
-#endif
     }
     
     /// <summary>
@@ -155,16 +177,13 @@ namespace QFramework
     }
 
 
-#if UNITY_5_6_OR_NEWER
     public class OnPropertyChangedEvent<T> : UnityEvent<T>
     {
 
     }
-#endif
 
- 
 
-#if UNITY_5_6_OR_NEWER
+
     public class PlayerPrefsBooleanProperty : Property<bool>
     {
         public PlayerPrefsBooleanProperty(string saveKey, bool defaultValue = false)
@@ -212,7 +231,6 @@ namespace QFramework
             this.Bind(value => PlayerPrefs.SetString(saveKey, value));
         }
     }
-#endif
 
     /// <summary>
     /// 
@@ -262,7 +280,5 @@ namespace QFramework
                 if (mValueSetter != null) mValueSetter(value);
             }
         }
-
-
     }
 }
