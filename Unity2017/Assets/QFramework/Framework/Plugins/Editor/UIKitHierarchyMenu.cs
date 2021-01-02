3b2f870351333e72204d21ca81b5b9afed571fd2
liangxie <liangxiegame@163.com>
2021-01-02 13:08:13 +0800

    v0.11.20 UIKit & AudioKit => dll

---
+++ Unity2017/Assets/QFramework/Framework/Plugins/Editor/UIKitHierarchyMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace QFramework
+{
+    public static class UIKitHierarchyMenu
+    {
+        [MenuItem("GameObject/@UI Kit - Add Bind (alt + b) &b",false,-1)]
+        public static void AddBind()
+        {
+            foreach (var o in Selection.objects.OfType<GameObject>())
+            {
+                if (o)
+                {
+                    var uiMark = o.GetComponent<Bind>();
+
+                    if (!uiMark)
+                    {
+                        o.AddComponent<Bind>();
+                    }
+                    
+                    EditorUtils.MarkCurrentSceneDirty();
+                }
+            }
+        }
+    }
+}
