48296058c55e5042936e0ebd49fa99efb92a2126
liangxie <liangxiegame@163.com>
2021-08-15 15:13:14 +0800

    删除掉 SubProject

--- Unity2018Test/Assets/QFramework/Extensions/ActionKit/ActionKitUniRxAction/Example/ActionKitUniRxActionExample.cs
+++ Unity2018Test/Assets/QFramework/Extensions/ActionKit/ActionKitUniRxAction/Example/ActionKitUniRxActionExample.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/****************************************************************************
+ * Copyright (c) 2021.8 liangxiegame MIT License
+ * 
+ * http://qframework.io
+ * https://github.com/liangxiegame/QFramework
+ ****************************************************************************/
+
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ActionKitUniRxActionExample : MonoBehaviour
+namespace QFramework.Example
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ActionKitUniRxActionExample : MonoBehaviour
     {
-        
-    }
+        void Start()
+        {
+            var btn1 = transform.Find("Btn1").GetComponent<Button>();
+            var btn2 = transform.Find("Btn2").GetComponent<Button>();
+            var btn3 = transform.Find("Btn3").GetComponent<Button>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            btn1.Hide();
+            btn2.Hide();
+            btn3.Hide();
+
+            this.Sequence()
+                .UniRx(() =>
+                {
+                    btn1.Show();
+                    return btn1.OnClickAsObservable().First().Do(_ => btn1.Hide());
+                })
+                .UniRx(() =>
+                {
+                    btn2.Show();
+                    return btn2.OnClickAsObservable().First().Do(_ => btn2.Hide());
+                })
+                .UniRx(() =>
+                {
+                    btn3.Show();
+                    return btn3.OnClickAsObservable().First().Do(_ => btn3.Hide());
+                })
+                .Begin();
+        }
     }
 }
