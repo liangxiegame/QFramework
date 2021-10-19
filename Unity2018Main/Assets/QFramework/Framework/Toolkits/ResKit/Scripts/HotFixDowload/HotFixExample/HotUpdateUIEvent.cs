0f0faad56a666be83f05550155a7021fc8844a1b
h3166179 <1178092718@qq.com>
2021-10-19 18:54:06 +0800

    迁移旧版代码，剥离依赖

---
+++ Unity2018Main/Assets/QFramework/Framework/Toolkits/ResKit/Scripts/HotFixDowload/HotFixExample/HotUpdateUIEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QFramework
+{
+    public class HotUpdateUIEvent : MonoBehaviour
+    {
+        public Image HotFixedSlider;
+        public Text HotSliderSpeedText;
+        public Text HotSliderInfoText;
+        public Text HotContenText;
+        public Text TitleText;
+        public Text HotFixDesText;
+        public Button ConfirmBtn;
+        public Button CancleBtn;
+
+        public void Show(string title, string str, UnityEngine.Events.UnityAction confirmAction, UnityEngine.Events.UnityAction cancleAction)
+        {
+   
+            TitleText.text = title;
+            HotFixDesText.text = str;
+
+            ConfirmBtn.onClick.AddListener(()=>confirmAction());
+
+            CancleBtn.onClick.AddListener(() => cancleAction());
+        }
+    }
+}
+
