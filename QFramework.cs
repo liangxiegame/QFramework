ff1f044b9e4813ac23b6dec594e8997a2987af82
liangxie <liangxiegame@163.com>
2021-12-04 12:04:35 +0800

    v1.0.0

---
+++ QFramework.cs
@@ -0,0 +1,704 @@
+/****************************************************************************
+ * Copyright (c) 2015 ~ 2022 liangxiegame MIT License
+ *
+ * QFramework v1.0
+ *
+ * https://qframework.cn
+ * https://github.com/liangxiegame/QFramework
+ * 
+ * Author:
+ *  liangxie
+ *  王二 soso
+ * Contributor
+ *  TastSong
+ * 
+ * Community
+ *  QQ Group: 623597263
+ ****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QFramework
+{
+    #region Architecture
+    public interface IArchitecture
+    {
+        void RegisterSystem<T>(T system) where T : ISystem;
+
+        void RegisterModel<T>(T model) where T : IModel;
+
+        void RegisterUtility<T>(T utility) where T : IUtility;
+
+        T GetSystem<T>() where T : class, ISystem;
+
+        T GetModel<T>() where T : class, IModel;
+
+        T GetUtility<T>() where T : class, IUtility;
+
+        void SendCommand<T>() where T : ICommand, new();
+        void SendCommand<T>(T command) where T : ICommand;
+
+        TResult SendQuery<TResult>(IQuery<TResult> query);
+        
+        void SendEvent<T>() where T : new();
+        void SendEvent<T>(T e);
+
+        IUnRegister RegisterEvent<T>(Action<T> onEvent);
+        void UnRegisterEvent<T>(Action<T> onEvent);
+    }
+
+    public abstract class Architecture<T> : IArchitecture where T : Architecture<T>, new()
+    {
+        /// <summary>
+        /// 是否初始化完成 
+        /// </summary>
+        private bool mInited = false;
+
+        private List<ISystem> mSystems = new List<ISystem>();
+
+        private List<IModel> mModels = new List<IModel>();
+
+        public static Action<T> OnRegisterPatch = architecture => { };
+
+        private static T mArchitecture;
+
+        public static IArchitecture Interface
+        {
+            get
+            {
+                if (mArchitecture == null)
+                {
+                    MakeSureArchitecture();
+                }
+
+                return mArchitecture;
+            }
+        }
+
+
+        static void MakeSureArchitecture()
+        {
+            if (mArchitecture == null)
+            {
+                mArchitecture = new T();
+                mArchitecture.Init();
+
+                OnRegisterPatch?.Invoke(mArchitecture);
+
+                foreach (var architectureModel in mArchitecture.mModels)
+                {
+                    architectureModel.Init();
+                }
+
+                mArchitecture.mModels.Clear();
+
+                foreach (var architectureSystem in mArchitecture.mSystems)
+                {
+                    architectureSystem.Init();
+                }
+
+                mArchitecture.mSystems.Clear();
+
+                mArchitecture.mInited = true;
+            }
+        }
+
+        protected abstract void Init();
+
+        private IOCContainer mContainer = new IOCContainer();
+
+        public void RegisterSystem<TSystem>(TSystem system) where TSystem : ISystem
+        {
+            system.SetArchitecture(this);
+            mContainer.Register<TSystem>(system);
+
+            if (!mInited)
+            {
+                mSystems.Add(system);
+            }
+            else
+            {
+                system.Init();
+            }
+        }
+
+        public void RegisterModel<TModel>(TModel model) where TModel : IModel
+        {
+            model.SetArchitecture(this);
+            mContainer.Register<TModel>(model);
+
+            if (!mInited)
+            {
+                mModels.Add(model);
+            }
+            else
+            {
+                model.Init();
+            }
+        }
+
+        public void RegisterUtility<TUtility>(TUtility utility) where TUtility : IUtility
+        {
+            mContainer.Register<TUtility>(utility);
+        }
+
+        public TSystem GetSystem<TSystem>() where TSystem : class, ISystem
+        {
+            return mContainer.Get<TSystem>();
+        }
+
+        public TModel GetModel<TModel>() where TModel : class, IModel
+        {
+            return mContainer.Get<TModel>();
+        }
+
+        public TUtility GetUtility<TUtility>() where TUtility : class, IUtility
+        {
+            return mContainer.Get<TUtility>();
+        }
+
+        public void SendCommand<TCommand>() where TCommand : ICommand, new()
+        {
+            var command = new TCommand();
+            command.SetArchitecture(this);
+            command.Execute();
+        }
+
+        public void SendCommand<TCommand>(TCommand command) where TCommand : ICommand
+        {
+            command.SetArchitecture(this);
+            command.Execute();
+        }
+
+        public TResult SendQuery<TResult>(IQuery<TResult> query)
+        {
+            query.SetArchitecture(this);
+            return query.Do();
+        }
+
+        private ITypeEventSystem mTypeEventSystem = new TypeEventSystem();
+        
+        public void SendEvent<TEvent>() where TEvent : new()
+        {
+            mTypeEventSystem.Send<TEvent>();            
+        }
+
+        public void SendEvent<TEvent>(TEvent e)
+        {
+            mTypeEventSystem.Send<TEvent>(e);            
+        }
+
+        public IUnRegister RegisterEvent<TEvent>(Action<TEvent> onEvent)
+        {
+            return mTypeEventSystem.Register<TEvent>(onEvent);
+        }
+
+        public void UnRegisterEvent<TEvent>(Action<TEvent> onEvent)
+        {
+            mTypeEventSystem.UnRegister<TEvent>(onEvent);
+        }
+    }
+    #endregion
+
+    #region Controller
+
+    public interface IController : IBelongToArchitecture,ICanSendCommand,ICanGetSystem,ICanGetModel,ICanRegisterEvent,ICanSendQuery
+    {
+
+    }
+    #endregion
+
+    #region System
+    public interface ISystem : IBelongToArchitecture ,ICanSetArchitecture,ICanGetModel,ICanGetUtility,ICanRegisterEvent,ICanSendEvent,ICanGetSystem
+    {
+        void Init();
+    }
+
+    public abstract class AbstractSystem : ISystem
+    {
+        private IArchitecture mArchitecture;
+        IArchitecture IBelongToArchitecture.GetArchitecture()
+        {
+            return mArchitecture;
+        }
+
+        void ICanSetArchitecture.SetArchitecture(IArchitecture architecture)
+        {
+            mArchitecture = architecture;
+        }
+
+        void ISystem.Init()
+        {
+            OnInit();
+        }
+
+        protected abstract void OnInit();
+    }
+    
+
+    #endregion
+
+    #region Model
+    public interface IModel : IBelongToArchitecture,ICanSetArchitecture,ICanGetUtility,ICanSendEvent
+    {
+        void Init();
+    }
+
+    public abstract class AbstractModel: IModel
+    {
+        private IArchitecture mArchitecturel;
+        
+        IArchitecture IBelongToArchitecture.GetArchitecture()
+        {
+            return mArchitecturel;
+        }
+
+        void ICanSetArchitecture.SetArchitecture(IArchitecture architecture)
+        {
+            mArchitecturel = architecture;
+        }
+
+        void IModel.Init()
+        {
+            OnInit();
+        }
+
+        protected abstract void OnInit();
+    }
+    
+
+    #endregion
+
+    #region Utility
+    public interface IUtility
+    {
+    }
+    #endregion
+
+    #region Command
+    public interface ICommand : IBelongToArchitecture,ICanSetArchitecture,ICanGetSystem,ICanGetModel,ICanGetUtility,ICanSendEvent,ICanSendCommand,ICanSendQuery
+    {
+        void Execute();
+    }
+
+    public abstract class AbstractCommand : ICommand
+    {
+        private IArchitecture mArchitecture;
+        IArchitecture IBelongToArchitecture.GetArchitecture()
+        {
+            return mArchitecture;
+        }
+
+        void ICanSetArchitecture.SetArchitecture(IArchitecture architecture)
+        {
+            mArchitecture = architecture;
+        }
+
+        void ICommand.Execute()
+        {
+            OnExecute();
+        }
+
+        protected abstract void OnExecute();
+    }
+    
+
+    #endregion
+    
+    #region Query
+
+    public interface IQuery<TResult> : IBelongToArchitecture,ICanSetArchitecture,ICanGetModel,ICanGetSystem,ICanSendQuery
+    {
+        TResult Do();
+    }
+    
+    public abstract class AbstractQuery<T> : IQuery<T>
+    {
+        public T Do()
+        {
+            return OnDo();
+        }
+
+        protected abstract T OnDo();
+
+
+        private IArchitecture mArchitecture;
+        
+        public IArchitecture GetArchitecture()
+        {
+            return mArchitecture;
+        }
+
+        public void SetArchitecture(IArchitecture architecture)
+        {
+            mArchitecture = architecture;
+        }
+    }
+
+    #endregion
+
+    #region Rule
+    public interface IBelongToArchitecture
+    {
+        IArchitecture GetArchitecture();
+    }
+    
+    public interface ICanSetArchitecture
+    {
+        void SetArchitecture(IArchitecture architecture);
+    }
+    
+    public interface ICanGetModel : IBelongToArchitecture
+    {
+    }
+    
+    public static class CanGetModelExtension
+    {
+        public static T GetModel<T>(this ICanGetModel self) where T : class, IModel
+        {
+            return self.GetArchitecture().GetModel<T>();
+        }
+    }
+    
+    public interface ICanGetSystem : IBelongToArchitecture
+    {
+        
+    }
+    
+    public static class CanGetSystemExtension
+    {
+        public static T GetSystem<T>(this ICanGetSystem self) where T : class, ISystem
+        {
+            return self.GetArchitecture().GetSystem<T>();
+        }
+    }
+    
+    public interface ICanGetUtility : IBelongToArchitecture
+    {
+
+    }
+
+    public static class CanGetUtilityExtension
+    {
+        public static T GetUtility<T>(this ICanGetUtility self) where T : class, IUtility
+        {
+            return self.GetArchitecture().GetUtility<T>();
+        }
+    }
+    
+    public interface ICanRegisterEvent : IBelongToArchitecture
+    {
+    }
+
+    public static class CanRegisterEventExtension
+    {
+        public static IUnRegister RegisterEvent<T>(this ICanRegisterEvent self, Action<T> onEvent)
+        {
+            return self.GetArchitecture().RegisterEvent<T>(onEvent);
+        }
+        
+        public static void UnRegisterEvent<T>(this ICanRegisterEvent self, Action<T> onEvent)
+        {
+            self.GetArchitecture().UnRegisterEvent<T>(onEvent);
+        }
+    }
+    
+    public interface ICanSendCommand : IBelongToArchitecture
+    {
+
+    }
+
+    public static class CanSendCommandExtension
+    {
+        public static void SendCommand<T>(this ICanSendCommand self) where T : ICommand, new()
+        {
+            self.GetArchitecture().SendCommand<T>();
+        }
+        
+        public static void SendCommand<T>(this ICanSendCommand self,T command) where T : ICommand
+        {
+            self.GetArchitecture().SendCommand<T>(command);
+        }
+    }
+    
+    public interface ICanSendEvent : IBelongToArchitecture
+    {
+    }
+
+    public static class CanSendEventExtension
+    {
+        public static void SendEvent<T>(this ICanSendEvent self) where T : new()
+        {
+            self.GetArchitecture().SendEvent<T>();
+        }
+
+        public static void SendEvent<T>(this ICanSendEvent self, T e)
+        {
+            self.GetArchitecture().SendEvent<T>(e);
+        }
+    }
+    
+    public interface ICanSendQuery : IBelongToArchitecture
+    {
+        
+    }
+
+    public static class CanSendQueryExtension
+    {
+        public static TResult SendQuery<TResult>(this ICanSendQuery self, IQuery<TResult> query)
+        {
+            return self.GetArchitecture().SendQuery(query);
+        }
+    }
+    #endregion
+
+    #region TypeEventSystem
+
+     public interface ITypeEventSystem
+    {
+        void Send<T>() where T : new();
+        void Send<T>(T e);
+        IUnRegister Register<T>(Action<T> onEvent);
+        void UnRegister<T>(Action<T> onEvent);
+    }
+
+    public interface IUnRegister
+    {
+        void UnRegister();
+    }
+
+    public struct TypeEventSystemUnRegister<T> : IUnRegister
+    {
+        public ITypeEventSystem TypeEventSystem;
+        public Action<T> OnEvent;
+        
+        public void UnRegister()
+        {
+            TypeEventSystem.UnRegister<T>(OnEvent);
+
+            TypeEventSystem = null;
+
+            OnEvent = null;
+        }
+    }
+
+    public class UnRegisterOnDestroyTrigger : MonoBehaviour
+    {
+        private HashSet<IUnRegister> mUnRegisters = new HashSet<IUnRegister>();
+
+        public void AddUnRegister(IUnRegister unRegister)
+        {
+            mUnRegisters.Add(unRegister);
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var unRegister in mUnRegisters)
+            {
+                unRegister.UnRegister();
+            }
+            
+            mUnRegisters.Clear();
+        }
+    }
+
+    public static class UnRegisterExtension
+    {
+        public static void UnRegisterWhenGameObjectDestroyed(this IUnRegister unRegister, GameObject gameObject)
+        {
+            var trigger = gameObject.GetComponent<UnRegisterOnDestroyTrigger>();
+
+            if (!trigger)
+            {
+                trigger = gameObject.AddComponent<UnRegisterOnDestroyTrigger>();
+            }
+
+            trigger.AddUnRegister(unRegister);
+        }
+    }
+    
+    public class TypeEventSystem  : ITypeEventSystem
+    {
+        public interface IRegistrations
+        {
+            
+        }
+        
+        public class Registrations<T> : IRegistrations
+        {
+            public Action<T> OnEvent = e => { };
+        }
+
+        private Dictionary<Type, IRegistrations> mEventRegistration = new Dictionary<Type, IRegistrations>();
+
+
+        public void Send<T>() where T : new()
+        {
+            var e = new T();
+            Send<T>(e);
+        }
+
+        public void Send<T>(T e)
+        {
+            var type = typeof(T);
+            IRegistrations registrations;
+
+            if (mEventRegistration.TryGetValue(type, out registrations))
+            {
+                (registrations as Registrations<T>).OnEvent(e);
+            }
+        }
+
+        public IUnRegister Register<T>(Action<T> onEvent)
+        {
+            var type = typeof(T);
+            IRegistrations registrations;
+
+            if (mEventRegistration.TryGetValue(type, out registrations))
+            {
+                
+            }
+            else
+            {
+                registrations = new Registrations<T>();
+                mEventRegistration.Add(type, registrations);
+            }
+
+            (registrations as Registrations<T>).OnEvent += onEvent;
+
+            return new TypeEventSystemUnRegister<T>()
+            {
+                OnEvent = onEvent,
+                TypeEventSystem = this
+            };
+        }
+
+        public void UnRegister<T>(Action<T> onEvent)
+        {
+            var type = typeof(T);
+            IRegistrations registrations;
+
+            if (mEventRegistration.TryGetValue(type, out registrations))
+            {
+                (registrations as Registrations<T>).OnEvent -= onEvent;
+            }
+        }
+    }
+
+    #endregion
+
+    #region IOC
+    public class IOCContainer
+    {
+        private Dictionary<Type, object> mInstances = new Dictionary<Type, object>();
+
+        public void Register<T>(T instance)
+        {
+            var key = typeof(T);
+
+            if (mInstances.ContainsKey(key))
+            {
+                mInstances[key] = instance;
+            }
+            else
+            {
+                mInstances.Add(key, instance);
+            }
+        }
+
+        public T Get<T>() where T : class
+        {
+            var key = typeof(T);
+
+            if (mInstances.TryGetValue(key, out var retInstance))
+            {
+                return retInstance as T;
+            }
+
+            return null;
+        }
+    }
+    
+
+    #endregion
+
+    #region BindableProperty
+    public class BindableProperty<T>
+    {
+
+        public BindableProperty(T defaultValue = default)
+        {
+            mValue = defaultValue;
+        }
+
+        private T mValue = default(T);
+
+        public T Value
+        {
+            get => mValue;
+            set
+            {
+                if (value == null && mValue == null) return;
+                if (value != null && value.Equals(mValue)) return;
+                
+                mValue = value;
+                mOnValueChanged?.Invoke(value);
+            }
+        }
+        
+        private Action<T> mOnValueChanged = (v) => { }; 
+
+        public IUnRegister Register(Action<T> onValueChanged) 
+        {
+            mOnValueChanged += onValueChanged;
+            return new BindablePropertyUnRegister<T>()
+            {
+                BindableProperty = this,
+                OnValueChanged = onValueChanged
+            };
+        }
+        
+        public IUnRegister RegisterWithInitValue(Action<T> onValueChanged)
+        {
+            onValueChanged(mValue);
+            return Register(onValueChanged);
+        }
+
+        public static implicit operator T(BindableProperty<T> property)
+        {
+            return property.Value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        public void UnRegister(Action<T> onValueChanged)
+        {
+            mOnValueChanged -= onValueChanged;
+        }
+
+    }
+    
+    public class BindablePropertyUnRegister<T> : IUnRegister
+    {
+        
+        public BindableProperty<T> BindableProperty { get; set; }
+      
+        public Action<T> OnValueChanged { get; set; }
+      
+        public void UnRegister()
+        {
+            BindableProperty.UnRegister(OnValueChanged);
+
+            BindableProperty = null;
+            OnValueChanged = null;
+        }
+    }
+    
+    #endregion
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,213 @@
-# QFramework
+
+
+
+
+[![](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/liangxiegame/QFramework/blob/master/LICENSE)
+[![Build Status](https://travis-ci.org/liangxiegame/QFramework.svg?branch=master)](https://travis-ci.org/liangxiegame/QFramework)
+
+# QFramework 简介 Intro
+
+[中文]()|[English](README_EN.md)
+
+[QFramework](https://github.com/liangxiegame/QFramework) 是提供一套简单、强大、易上手、符合 SOLID 原则、支持领域驱动设计（DDD）、事件驱动、数据驱动、分层、MVC 、CQRS、模块化、易扩展的架构，她的源码只有不到 800 行，你可以完全把她放在你的笔记应用里保存。
+
+## 提供的架构图
+
+![](http://processon.com/chart_image/5c270aa6e4b007ba5d5029dc.png)
+
+## 示意图
+
+![](http://processon.com/chart_image/5cbb1edce4b0bab90960a4f6.png)
+
+
+
+### 运行环境
+
+* Unity 2018.4.x ~ 2021.x
+
+## 安装
+
+* 从 [Asset Store](http://u3d.as/SJ9) 下载
+* PackageManager
+    * add from package git url：https://github.com/liangxiegame/QFramework.git 
+    * 或者国内镜像仓库：https://gitee.com/liangxiegame/QFramework.git
+* 或者直接复制[此代码](QFramework.cs)到自己项目中的任意脚本中
+* OpenUPM(TODO)
+
+
+
+## 资源
+
+| 名称                     | 描述                                     | 地址                                                         |
+| ----------------------  | ---------------------------------------- | ------------------------------------------------------------ |
+| Example 示例 | 包含 CounterApp | [github](https://github.com/liangxiegame/QFramework.Example)\|[gitee](https://gitee.com/liangxiegame/QFramework.Example) |
+| ------**社区**------ |  |  |
+| QQ 群:623597263        | 交流群 | [点击加群](http://shang.qq.com/wpa/qunwpa?idkey=706b8eef0fff3fe4be9ce27c8702ad7d8cc1bceabe3b7c0430ec9559b3a9ce66) |
+| github issue | github 社区 | [地址](https://github.com/liangxiegame/QFramework/issues/new) |
+| gitee issue | gitee 社区（国内访问快） | [地址](https://gitee.com/liangxiegame/QFramework/issues) |
+| ------**教程**------ |  |  |
+| 教程《框架搭建 决定版》    | 教程 QFramework  的核心架构是怎么演化过来的？ | [课程主页](https://learn.u3d.cn/tutorial/framework_design)   |
+| ------**案例**------ |  |  |
+| 独立游戏《鬼山之下》   | 使用 QF 制作的独立游戏           | [游戏主页(Steam)](https://store.steampowered.com/app/1517160/_/) |
+| 手机游戏《谐音梗挑战》 | 使用 QF 制作的手机游戏          | [游戏主页(TapTap)](https://www.taptap.com/app/201075)        |
+
+
+
+## Star 趋势（如果项目有帮到您欢迎点赞）
+
+[![Stargazers over time](https://starchart.cc/liangxiegame/QFramework.svg)](https://starchart.cc/liangxiegame/QFramework)
+
+### 核心成员
+
+* [h3166179](https://github.com/h3166179)
+* [王二](https://github.com/so-sos-so) [so-sos-so](https://github.com/so-sos-so)
+
+* [凉鞋 liangxieq](https://github.com/liangxieq)
+
+
+
+
+### 优秀的 Unity 库、框架
+
+- [ET](https://github.com/egametang/ET)：ET Unity3D Client And C# Server Framework
+- [IFramework（OnClick）](https://github.com/OnClick9927/IFramework) Simple Unity Tools
+- [JEngine](https://github.com/JasonXuDeveloper/JEngine)  一个基于XAsset&ILRuntime，精简好用的热更框架
+
+### 代码规范完全遵循:
+
+[QCSharpStyleGuide](https://github.com/liangxiegame/QCSharpStyleGuide)
+
+
+### 赞助 Donate:
+
+* 如果觉得不错可以在 [这里 Asset Store](http://u3d.as/SJ9) 给个 5 星哦~ give 5 star
+* 或者给此仓库一个小小的 Star~ star this repository
+* 以上这些都会转化成我们的动力,提供更好的技术服务! 
+
+
+## TODO:
+
+以下内容待整理内容
+
+
+
+#### 快速开始 QuickStart:
+
+**1.Action Kit**
+
+* chainning style(Driven by MonoBehaviour or Update)
+
+``` csharp
+this.Sequence()
+	.Delay(1.0f)
+	.Event(()=>Log.I("Delayed 1 second"))
+	.Until(()=>something is done)
+	.Begin();
+```
+
+* object oriented style
+
+``` csharp
+var sequenceNode = new SequenceNode();
+sequenceNode.Append(DelayAction.Allocate(1.0f));
+sequenceNode.Append(EventAction.Allocate(()=>Log.I("Delayed 1 second"));
+sequenceNode.Append(UntilAction.Allocate(()=>something is true));
+
+this.ExecuteNode(sequenceNode);
+```
+
+**2.Res Kit**
+``` csharp
+// allocate a loader when initialize a panel or a monobehavour
+var loader = ResLoader.Allocate();
+
+// load someth in a panel or a monobehaviour
+loader.LoadSync<GameObject>("resources://smobj");
+
+loader.LoadSync<Texture2D>("resources://Bg");
+
+// load by asset bundle's assetName
+loader.LoadSync<Texture2D>("HomeBg");
+
+// load by asset bundle name and assetName
+loader.LoadSync<Texture2D>("home","HomeBg");
+
+
+// resycle this panel/monobehaivour's loaded res when destroyed 
+loader.Recycle2Cache();
+loader = null;
+```
+
+**3.UI Kit**
+
+``` csharp
+// open a panel from assetBundle
+UIKit.OpenPanel<UIMainPanel>();
+
+// load a panel from specified Resources
+UIKit.OpenPanel<UIMainPanel>(prefabName:"Resources/UIMainPanel");
+
+// load a panel from specield assetName
+UIKit.OpenPanel<UIMainPanel>(prefabName:"UIMainPanel1");
+```
+
+### 技术支持 Tech Support：
+* [文档 Document:http://qf.liangxiegame.com/qf/community](http://qf.liangxiegame.com/qf/community)
+* **社区:https://qframework.cn**
+* [awesome_qframework](https://github.com/liangxiegame/awesome-qframework)  
+
+#### 下载地址 Download:
+* 最新版本:https://github.com/liangxiegame/QFramework/releases
+* 
+
+### 可选的包含项目 Include Projects:
+* [UniRx](https://github.com/neuecc/UniRx)
+* [Json.net](https://github.com/JamesNK/Newtonsoft.Json)
+
+#### 参考 Reference:
+* [MultyFramework](https://github.com/OnClick9927/MultyFramework)
+* [IFramework_GUICanvas](https://github.com/OnClick9927/IFramework_GUICanvas)
+* [IFramework](https://github.com/OnClick9927/IFramework)
+* [Loxodon Framework](https://github.com/cocowolf/loxodon-framework)
+* [BDFramework](https://github.com/yimengfan/BDFramework.Core)
+* [HGFramework: Unity3D客户端框架](https://github.com/zhutaorun/HGFramework)
+* [Qarth: Framework For Game Develop With Unity3d](https://github.com/SnowCold/Qarth)
+* [GameFramework:A game framework based on Unity 5.3 and later versions](https://github.com/EllanJiang/GameFramework)
+* [cocos2d/cocos2d-x](https://github.com/cocos2d/cocos2d-x)
+* [ResetCore.Unity](https://github.com/vgvgvvv/ResetCore.Unity)
+* [UnityUGUIImageShaderPack](https://github.com/zhangmaker/UnityUGUIImageShaderPack)
+* [FishManShaderTutorial](https://github.com/JiepengTan/FishManShaderTutorial)
+
+
+
+## 功能列表
+
+| 模块名称  | 包含功能                  | 描述                                                         |
+| --------- | ------------------------- | ------------------------------------------------------------ |
+| Core      | Architecture              | 一套全栈通用的系统设计架构，QF 的本身是用这套架构设计，同时这套架构可以应用与项目开发，为 QF 的主要提供架构，QF 的文档系统（React）、QF 的插件系统（.Net Core）均用此架构开发。 |
+|           | CodeGen                   | 代码生成库，一套链式代码生成模板。                           |
+|           | Singleton                 | 一套单例模板工具                                             |
+|           | IOC                       | 依赖注入/控制反转容器                                        |
+|           | Event                     | 事件机制实现，包含枚举事件 和 类型事件                       |
+|           | Pool                      | 各种对象池提供，包含 List、Dictionary 对象池                 |
+|           | FSM                       | 一套基于类型的状态机实现                                     |
+|           | Disposable                | 销毁模式 和 对应扩展方法实现                                 |
+|           | Factory                   | 对象的创建模式封装                                           |
+|           | RefCounter                | 引用计数器实现                                               |
+|           | CSharpExtensions          | 大量的方便易用的扩展实现                                     |
+|           | Table                     | 可以建立索引的表格数据结构实现                               |
+|           | EasyIMGUI                 | 方便易用的、面向对象的（组合模式）的 IMGUI（OnGUI）绘制库    |
+|           | Utility                   | 各种静态方法封装                                             |
+|           | ManagerOfManagers（弃用） | Manager Of Managers 架构的实现                               |
+| ActionKit | 之后写                    |                                                              |
+| ResKit    | SimulationModel           | 真机和编辑器模拟资源加载双模式，让开发阶段与真机阶段自如切换 |
+|           | ResPathLoad               | 从Resources目录与沙盒目录以及从网络中加载资源                |
+|           | LoadSprite                | 加载Sprite或精灵图集                                         |
+|           | LoadScene                 | 在AssetBundle中同步与异步加载Scene场景                       |
+|           | ResAssetManager           | 标记的AssetBunlde资源管理，快速定位                          |
+|           | LoadASync                 | 异步加载与异步队列加载                                       |
+|           | CustomRes                 | Reskit功能自定义拓展                                         |
+|           | CustomLoadConfig          | AssetBundle自定义配置表生成                                  |
+| UIKit     | 之后写                    |                                                              |
+| AudioKit  | 之后写                    |                                                              |
+
