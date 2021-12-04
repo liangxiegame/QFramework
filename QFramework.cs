01d0e86fe57320bfa007119bec7db27737035185
liangxie <liangxiegame@163.com>
2021-12-04 12:35:16 +0800

    add IOnEvent

--- QFramework.cs
+++ QFramework.cs
@@ -200,6 +200,24 @@ namespace QFramework
             mTypeEventSystem.UnRegister<TEvent>(onEvent);
         }
     }
+    
+    public interface IOnEvent<T>
+    {
+        void OnEvent(T e);
+    }
+
+    public static class OnGlobalEventExtension
+    {
+        public static IUnRegister RegisterEvent<T>(this IOnEvent<T> self) where T : struct
+        {
+            return TypeEventSystem.Global.Register<T>(self.OnEvent);
+        }
+
+        public static void UnRegisterEvent<T>(this IOnEvent<T> self) where T : struct
+        {
+            TypeEventSystem.Global.UnRegister<T>(self.OnEvent);
+        }
+    }
     #endregion
 
     #region Controller
