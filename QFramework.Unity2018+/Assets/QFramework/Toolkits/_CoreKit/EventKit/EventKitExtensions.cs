cb638148160fe8cf18c8e7040f61f3b50f295353
LIANGXIEWIN\liang <liangxiegame@163.com>
2022-12-19 14:28:51 +0800

    v1.0.24 EventKit 增加 Component 版本的 UnRegisterWhenGameObjectDestroy API

---
+++ QFramework.Unity2018+/Assets/QFramework/Toolkits/_CoreKit/EventKit/EventKitExtensions.cs
@@ -0,0 +1,21 @@
+﻿/****************************************************************************
+ * Copyright (c) 2016 - 2022 liangxiegame UNDER MIT License
+ * 
+ * https://qframework.cn
+ * https://github.com/liangxiegame/QFramework
+ * https://gitee.com/liangxiegame/QFramework
+ ****************************************************************************/
+
+using UnityEngine;
+
+namespace QFramework
+{
+    public static class EventKitExtensions 
+    {
+        public static IUnRegister UnRegisterWhenGameObjectDestroyed<T>(this IUnRegister self, T component)
+            where T : Component
+        {
+            return self.UnRegisterWhenGameObjectDestroyed(component.gameObject);
+        }
+    }
+}
