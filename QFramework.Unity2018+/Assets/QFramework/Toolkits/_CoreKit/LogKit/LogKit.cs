e5854677b0804c83895a78e541d062c0abe8c9e2
liangxiegame <liangxiegame@163.com>
2023-11-10 13:58:14 +0800

    v1.0.109 LogKit 支持双击找到报错定位（感谢 clksaaa 提供的反馈和解决方案)

--- QFramework.Unity2018+/Assets/QFramework/Toolkits/_CoreKit/LogKit/LogKit.cs
+++ QFramework.Unity2018+/Assets/QFramework/Toolkits/_CoreKit/LogKit/LogKit.cs
@@ -1,11 +1,16 @@
 /****************************************************************************
- * Copyright (c) 2015 - 2022 liangxiegame UNDER MIT License
- * 
- * http://qframework.io
+ * Copyright (c) 2016 - 2023 liangxiegame UNDER MIT License
+ *
+ * https://qframework.cn
  * https://github.com/liangxiegame/QFramework
+ * https://gitee.com/liangxiegame/QFramework
+ * 
  ****************************************************************************/
 
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
+using UnityEditor;
 
 namespace QFramework
 {
@@ -49,7 +54,7 @@ LogKit.I(""Hello LogKit {0}{1}"",1,2);
                 Debug.LogFormat(msg.ToString(), args);
             }
         }
-        
+
 #if UNITY_EDITOR
         // v1 No.158
         [MethodAPI]
@@ -64,8 +69,8 @@ LogKit.E(""Hello LogKit {0}{1}"",1,2);
 // Hello LogKit FluentAPI
 ")]
 #endif
-        
-        public static void W(object msg,params object[] args)
+
+        public static void W(object msg, params object[] args)
         {
             if (mLogLevel < LogLevel.Warning)
             {
@@ -74,7 +79,6 @@ LogKit.E(""Hello LogKit {0}{1}"",1,2);
 
             if (args == null || args.Length == 0)
             {
-
                 Debug.LogWarning(msg);
             }
             else
@@ -82,8 +86,8 @@ LogKit.E(""Hello LogKit {0}{1}"",1,2);
                 Debug.LogWarningFormat(msg.ToString(), args);
             }
         }
-        
-        
+
+
 #if UNITY_EDITOR
         // v1 No.159
         [MethodAPI]
@@ -116,7 +120,6 @@ LogKit.E(""Hello LogKit {0}{1}"",1,2);
         }
 
 
-        
 #if UNITY_EDITOR
         // v1 No.160
         [MethodAPI]
@@ -140,8 +143,8 @@ LogKit.E(""Hello LogKit {0}{1}"",1,2);
 
             Debug.LogException(e);
         }
-        
-        
+
+
 #if UNITY_EDITOR
         // v1 No.161
         [MethodAPI]
@@ -160,7 +163,7 @@ LogKit.Builder()
         {
             return new StringBuilder();
         }
-        
+
 
         public enum LogLevel
         {
@@ -207,7 +210,7 @@ LogKit.Level = LogKit.LogLevel.Max;
             self.Append("</color>");
             return self;
         }
-        
+
         public static void LogInfo<T>(this T selfMsg)
         {
             LogKit.I(selfMsg);
@@ -226,6 +229,82 @@ LogKit.Level = LogKit.LogLevel.Max;
         public static void LogException(this Exception selfExp)
         {
             LogKit.E(selfExp);
+        }
+    }
+
+
+    // * 参考: https://zhuanlan.zhihu.com/p/92291084
+    // 感谢 https://github.com/clksaaa 提供的 issue 反馈 和 解决方案
+    public static class OpenAssetLogLine
+    {
+        private static bool m_hasForceMono = false;
+
+        // 处理asset打开的callback函数
+        [UnityEditor.Callbacks.OnOpenAssetAttribute(-1)]
+        static bool OnOpenAsset(int instance, int line)
+        {
+            if (m_hasForceMono) return false;
+
+            // 自定义函数，用来获取log中的stacktrace，定义在后面。
+            string stack_trace = GetStackTrace();
+            // 通过stacktrace来定位是否是自定义的log，log中有LogKit/LogKit.cs，很好识别
+            if (!string.IsNullOrEmpty(stack_trace) && stack_trace.Contains("LogKit/LogKit.cs"))
+            {
+                // 正则匹配at xxx，在第几行
+                Match matches = Regex.Match(stack_trace, @"\(at (.+)\)", RegexOptions.IgnoreCase);
+                string pathline = "";
+                while (matches.Success)
+                {
+                    pathline = matches.Groups[1].Value;
+
+                    // 找到不是我们自定义log文件的那行，重新整理文件路径，手动打开
+                    if (!pathline.Contains("LogKit/LogKit.cs") && !string.IsNullOrEmpty(pathline))
+                    {
+                        int split_index = pathline.LastIndexOf(":");
+                        string path = pathline.Substring(0, split_index);
+                        line = Convert.ToInt32(pathline.Substring(split_index + 1));
+                        m_hasForceMono = true;
+                        //方式一
+                        AssetDatabase.OpenAsset(AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(path), line);
+                        m_hasForceMono = false;
+                        //方式二
+                        //string fullpath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("Assets"));
+                        // fullpath = fullpath + path;
+                        //  UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(fullpath.Replace('/', '\\'), line);
+                        return true;
+                    }
+
+                    matches = matches.NextMatch();
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        static string GetStackTrace()
+        {
+// 找到类UnityEditor.ConsoleWindow
+            var type_console_window = typeof(EditorWindow).Assembly.GetType("UnityEditor.ConsoleWindow");
+// 找到UnityEditor.ConsoleWindow中的成员ms_ConsoleWindow
+            var filedInfo =
+                type_console_window.GetField("ms_ConsoleWindow", BindingFlags.Static | BindingFlags.NonPublic);
+// 获取ms_ConsoleWindow的值
+            var ConsoleWindowInstance = filedInfo.GetValue(null);
+            if (ConsoleWindowInstance != null)
+            {
+                if ((object)EditorWindow.focusedWindow == ConsoleWindowInstance)
+                {
+// 找到类UnityEditor.ConsoleWindow中的成员m_ActiveText
+                    filedInfo = type_console_window.GetField("m_ActiveText",
+                        BindingFlags.Instance | BindingFlags.NonPublic);
+                    string activeText = filedInfo.GetValue(ConsoleWindowInstance).ToString();
+                    return activeText;
+                }
+            }
+
+            return null;
         }
     }
 }
